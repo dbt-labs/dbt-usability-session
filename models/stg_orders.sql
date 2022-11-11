77d18c21fab9d747e7075c5e971e0f068f25738a
product.sandbox+001@dbtlabs.com <product.sandbox+001@dbtlabs.com>
2022-11-11 23:14:28 +0000

    pancakes

---
+++ models/stg_orders.sql
@@ -0,0 +1,14 @@
+
+
+
+select
+    id as order_id,
+    location_id as location_id,
+    customer_id as customer_id,
+    ordered_at as order_timestamp,
+    order_total as order_total,
+    tax_paid as order_tax
+from raw.coffee_shop.orders
+
+
+
